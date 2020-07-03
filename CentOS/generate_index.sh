d67b08a7aae60234ea8563db66be7bd98e8c642c
Mounir Missaoui <mounir@fivepoints.fr>
2020-07-03 21:07:04 +0100

    index file and timestamp generation

--- CentOS/generate_index.sh
+++ CentOS/generate_index.sh
@@ -1,4 +1,8 @@
-<!doctype html>
+#!/usr/bin/env bash
+HTMLFILE="./index.html"
+TIMESTAMP=`date "+%s"`
+
+echo "<!doctype html>
 <html>
   <head>
     <title>It works !!</title>
@@ -6,6 +10,6 @@
   <body>
     <p>if you see this page , it means that this project is <strong>functional</strong>. </p>
     <br>
-    <p> this file was created at 1593806598 </p>
+    <p> this file was created at $TIMESTAMP </p>
   </body>
-</html> 
+</html> " > $HTMLFILE
